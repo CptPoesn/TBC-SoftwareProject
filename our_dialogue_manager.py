732bf735aff2b814a1d254dcc09a7aa58c717ceb
TheresaSchmidt <36304889+TheresaSchmidt@users.noreply.github.com>
2021-01-19 20:11:59 +0100

    Add files via upload

--- our_dialogue_manager.py
+++ our_dialogue_manager.py
@@ -16,46 +16,18 @@ import rasa.cli.run
 
 def process_input(input:str, model_path:str):
     # Get model
-    model = get_validated_path("../../Softwareprojekt/rasa_test/models", "model", DEFAULT_MODELS_PATH)
-    try:
-        model_path = get_model(model)
-    except ModelNotFound:
-        print(
-            "No model found. Train a model before running the "
-            "server using `rasa train`."
-        )
-        return
-    core_model, nlu_model = get_model_subdirectories(model_path)
-    # NLU model
+    model = get_validated_path(model_path, "model", DEFAULT_MODELS_PATH)
+    model_path = get_model(model)
+    _, nlu_model = get_model_subdirectories(model_path)
     interpreter = Interpreter.load(nlu_model)
 
     # compute NLU result
-    telemetry.track_shell_started("rasa")
     result = interpreter.parse(input)
+    # print out result
     print(json_to_string(result))
 
-    print(core_model)
-    # Compute response
-        # set default args as in scaffold.py
-    args = argparse.Namespace()
-    attributes = [
-        "endpoints",
-        "credentials",
-        "cors",
-        "auth_token",
-        "jwt_secret",
-        "jwt_method",
-        "enable_api",
-        "remote_storage",
-    ]
-    for a in attributes:
-        setattr(args, a, None)
 
-    args.port = constants.DEFAULT_SERVER_PORT
-    args.connector = "cmdline" # from shell.py
-    args.model = "../../Softwareprojekt/rasa_test/models"
-    rasa.cli.run.run(args)
 
-model_path = "../../Softwareprojekt/rasa_test"
+model_path = "../../Softwareprojekt/rasa_test/models"
 message = "hi there"
 process_input(message, model_path)
