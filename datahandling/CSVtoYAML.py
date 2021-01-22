ba377252283e517c3c36458cf580c452d58f3041
poesn <1337poesn@gmail.com>
2021-01-22 22:13:05 +0100

    First version of a csv to yaml/nlu file and the first version of the file. Todo: handle turnTakes differently (not just as intents.

--- datahandling/CSVtoYAML.py
+++ datahandling/CSVtoYAML.py
@@ -1,17 +1,58 @@
 import csv
+from collections import defaultdict as dd
 
-with open('sw00-0004_DiAML-MultiTab.csv') as csvfile:
-    reader = list(csv.DictReader(csvfile, delimiter = ';', dialect='excel'))
-    for rownum in range(len(reader)):
-        if reader[rownum]['Task']:
-            taskLabel = reader[rownum]['Task']
-            if 'inform' in taskLabel:
-                try:
-                    if reader[rownum]['Sender'] == reader[rownum + 1]['Sender']:
-                        reader[rownum]['Task'] = 'inform_continue'
-                    else:
-                        reader[rownum]['Task'] = 'inform_pass'
-                except:
-                    print('It happened, do something')
-    for row in reader:
-        print(row['Task'])
+def cvs_to_dict(filename):
+    with open(filename) as csvfile:
+        reader = list(csv.DictReader(csvfile, delimiter = ';', dialect='excel'))
+        for rownum in range(len(reader)):
+            for key, value in reader[rownum].items():
+                if value and key != 'Markables' and key != 'Sender' and key != 'Addressee' and key != 'Turn transcription' and key != 'FS text':
+                    reader[rownum][key] = value.split()[1]
+
+            if reader[rownum]['Task']:
+                taskLabel = reader[rownum]['Task']
+                if 'inform' in taskLabel:
+                    try:
+                        if reader[rownum]['Sender'] == reader[rownum + 1]['Sender']:
+                            reader[rownum]['Task'] = 'inform_continue'
+                        else:
+                            reader[rownum]['Task'] = 'inform_pass'
+                    except:
+                        print('Catch error that inform is last label')
+    return reader
+
+def dict_to_yaml(filename_out, data_dict):
+
+    yamlDict = dd(list)
+    for row in data_dict:
+        for key, value in row.items():
+            if value and key != 'Markables' and key != 'Sender' and key != 'Addressee' and key != 'Turn transcription' and key != 'FS text':
+                yamlDict[value].append(row['FS text'])
+    with open(filename_out, 'w') as f:
+        f.write('nlu:\n') #no spaces
+        for key, values in yamlDict.items():
+
+            f.write('  - intent: ' + key + '\n') #two spaces
+            f.write('  examples:' + '\n') #two spaces
+            for ex in values:
+                f.write('    - text: |\n') #four spaces
+                f.write('      ' + ex + '\n') #six spaces
+            f.write('\n')
+
+
+    # for row in reader:
+    #     str = ''
+    #     i = 0
+    #     for key, value in row.items():
+    #         if value and key != 'Markables' and key != 'Sender' and key != 'Addressee' and key != 'Turn transcription' and key != 'FS text':
+    #             i += 1
+    #             str += key + ': ' + value + ', '
+    #     print(str, i)
+
+if __name__ == '__main__':
+    file = 'sw00-0004_DiAML-MultiTab.csv'
+    file_out = file[:-3] + 'yaml'
+
+    data = cvs_to_dict(file)
+
+    dict_to_yaml(file_out, data)
