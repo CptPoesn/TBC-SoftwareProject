34d9b9facc417ea3ea97a4bd9656e96f30a95b1d
poesn <1337poesn@gmail.com>
2021-01-23 03:24:16 +0100

    Cleaned up the script.

--- datahandling/CSVtoYAML.py
+++ datahandling/CSVtoYAML.py
@@ -1,17 +1,20 @@
 import csv
 from collections import defaultdict as dd
 
+
 def cvs_to_dict(filename):
     with open(filename) as csvfile:
-        reader = list(csv.DictReader(csvfile, delimiter = ';', dialect='excel'))
+        reader = list(csv.DictReader(csvfile, delimiter=';', dialect='excel'))
         for rownum in range(len(reader)):
             for key, value in reader[rownum].items():
-                if value and key != 'Markables' and key != 'Sender' and key != 'Addressee' and key != 'Turn transcription' and key != 'FS text':
+                if value and key != 'Markables' and key != 'Sender' and key != 'Addressee' and key != 'Turn ' \
+                                                                                                      'transcription' \
+                        and key != 'FS text':
                     reader[rownum][key] = value.split()[1]
 
             if reader[rownum]['Task']:
-                taskLabel = reader[rownum]['Task']
-                if 'inform' in taskLabel:
+                task_label = reader[rownum]['Task']
+                if 'inform' in task_label:
                     try:
                         if reader[rownum]['Sender'] == reader[rownum + 1]['Sender']:
                             reader[rownum]['Task'] = 'inform_continue'
@@ -21,24 +24,25 @@ def cvs_to_dict(filename):
                         print('Catch error that inform is last label')
     return reader
 
-def dict_to_yaml(filename_out, data_dict):
 
-    yamlDict = dd(list)
+def dict_to_yaml(filename_out, data_dict):
+    yaml_dict = dd(list)
     for row in data_dict:
         for key, value in row.items():
-            if value and key != 'Markables' and key != 'Sender' and key != 'Addressee' and key != 'Turn transcription' and key != 'FS text':
-                yamlDict[value].append(row['FS text'])
+            if value and key != 'Markables' and key != 'Sender' \
+                    and key != 'Addressee' and key != 'Turn transcription' and key != 'FS text':
+                yaml_dict[value].append(row['FS text'])
     with open(filename_out, 'w') as f:
-        f.write('nlu:\n') #no spaces
-        for key, values in yamlDict.items():
+        f.write('nlu:\n')  # no spaces
+        for key, values in yaml_dict.items():
+            f.write('  - intent: ' + key + '\n')  # two spaces
+            f.write('  examples:' + '\n')  # two spaces
 
-            f.write('  - intent: ' + key + '\n') #two spaces
-            f.write('  examples:' + '\n') #two spaces
             for ex in values:
-                f.write('    - text: |\n') #four spaces
-                f.write('      ' + ex + '\n') #six spaces
-            f.write('\n')
+                f.write('    - text: |\n')  # four spaces
+                f.write('      ' + ex + '\n')  # six spaces
 
+            f.write('\n')
 
     # for row in reader:
     #     str = ''
@@ -49,10 +53,13 @@ def dict_to_yaml(filename_out, data_dict):
     #             str += key + ': ' + value + ', '
     #     print(str, i)
 
+
 if __name__ == '__main__':
     file = 'sw00-0004_DiAML-MultiTab.csv'
     file_out = file[:-3] + 'yaml'
 
     data = cvs_to_dict(file)
+
+    print(data[1].keys())
 
     dict_to_yaml(file_out, data)
