0253633e3935e3fb6a511fe42f6965180751f1ae
poesn <1337poesn@gmail.com>
2021-01-25 13:30:30 +0100

    added one instance of the TRAINS corpus, which is task oriented instead of dialogue based.

--- datahandling/CSVtoYAML.py
+++ datahandling/CSVtoYAML.py
@@ -7,9 +7,9 @@ def cvs_to_dict(filename):
         reader = list(csv.DictReader(csvfile, delimiter=';', dialect='excel'))
         for rownum in range(len(reader)):
             for key, value in reader[rownum].items():
-                if value and key != 'Markables' and key != 'Sender' and key != 'Addressee' and key != 'Turn ' \
+                if value and key != 'Markables' and key != 'Sender' and key != 'Speaker' and key != 'Addressee' and key != 'Turn ' \
                                                                                                       'transcription' \
-                        and key != 'FS text':
+                        and key != 'FS text' and key != 'Comments':
                     reader[rownum][key] = value.split()[1]
 
             if reader[rownum]['Task']:
@@ -29,19 +29,20 @@ def dict_to_yaml(filename_out, data_dict):
     yaml_dict = dd(list)
     for row in data_dict:
         for key, value in row.items():
+            # skips lines that aren't annotated
             if value and key != 'Markables' and key != 'Sender' \
                     and key != 'Addressee' and key != 'Turn transcription' and key != 'FS text' \
                     and key != 'turnManagement':
                 if row['turnManagement']:
                     yaml_dict[value].append(row['FS text'] + '\n      metadata:\n        turnManagement: '
                                             + row['turnManagement'])
-                    print(row['turnManagement'])
+                    # print(row['turnManagement'])
                 else:
                     yaml_dict[value].append(row['FS text'])
     with open(filename_out, 'w') as f:
         f.write('nlu:\n')  # no spaces
         for key, values in yaml_dict.items():
-            f.write('- intent: ' + key + '\n')  # two spaces
+            f.write('- intent: ' + key + '\n')  # no spaces
             f.write('  examples:' + '\n')  # two spaces
 
             for ex in values:
@@ -54,14 +55,15 @@ def dict_to_yaml(filename_out, data_dict):
     #     str = ''
     #     i = 0
     #     for key, value in row.items():
-    #         if value and key != 'Markables' and key != 'Sender' and key != 'Addressee' and key != 'Turn transcription' and key != 'FS text':
+    #         if value and key != 'Markables' and key != 'Sender' and key != 'Addressee' and \
+    #         key != 'Turn transcription' \and key != 'FS text':
     #             i += 1
     #             str += key + ': ' + value + ', '
     #     print(str, i)
 
 
 if __name__ == '__main__':
-    file = 'sw00-0004_DiAML-MultiTab.csv'
+    file = 'TRAINS-1-gold_standard-MultiTab-V21.csv'
     file_out = file[:-3] + 'yaml'
 
     data = cvs_to_dict(file)
