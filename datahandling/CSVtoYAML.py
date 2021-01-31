45f44dfdd34dc11ab6af21dc24af322b273015a4
poesn <1337poesn@gmail.com>
2021-01-31 03:55:58 +0100

    Added the remainding DialogueBank Corpora and converted them to yaml

--- datahandling/CSVtoYAML.py
+++ datahandling/CSVtoYAML.py
@@ -7,10 +7,11 @@ def cvs_to_dict(filename):
         reader = list(csv.DictReader(csvfile, delimiter=';', dialect='excel'))
         for rownum in range(len(reader)):
             for key, value in reader[rownum].items():
-                if value and key != 'Markables' and key != 'Sender' and key != 'Speaker' and key != 'Addressee' and key != 'Turn ' \
-                                                                                                      'transcription' \
-                        and key != 'FS text' and key != 'Comments':
-                    reader[rownum][key] = value.split()[1]
+                # TODO make list of allowed items instead of disallowed
+                if value and key != '﻿Markables' and key != 'Sender' and key != 'Addressee' and key != 'Turn transcription'and key != 'FS text' and key != 'Comments' and key!= 'other':
+                    spl = value.split()
+                    reader[rownum][key] = spl[1]
+
 
             if reader[rownum]['Task']:
                 task_label = reader[rownum]['Task']
@@ -20,7 +21,7 @@ def cvs_to_dict(filename):
                             reader[rownum]['Task'] = 'inform_continue'
                         else:
                             reader[rownum]['Task'] = 'inform_pass'
-                    except:
+                    except IndexError:
                         print('Catch error that inform is last label')
     return reader
 
@@ -63,13 +64,40 @@ def dict_to_yaml(filename_out, data_dict):
 
 
 if __name__ == '__main__':
+    # Examples/Tech Demo:
     # file = 'TRAINS-1-gold_standard-MultiTab-V21.csv'
-    file = 'sw00-0004_DiAML-MultiTab.csv'
+    # file = 'sw00-0004_DiAML-MultiTab.csv'
 
-    file_out = file[:-3] + 'yaml'
+    files = []
 
-    data = cvs_to_dict(file)
+    #Trains
+    files.append('csv/TRAINS/TRAINS-1-gold_standard-MultiTab-V21.csv')
+    files.append('csv/TRAINS/TRAINS-2-gold_standard-MultiTab-V21.csv')
+    files.append('csv/TRAINS/TRAINS-3-gold_standard-MultiTab-V2.csv')
 
-    print(data[1].keys())
+    #Switchboard
+    files.append('csv/Switchboard/sw00-0004_DiAML-MultiTab.xlsx.txt.csv')
+    files.append('csv/Switchboard/sw01-0105_DiAML-MultiTab.csv')
+    files.append('csv/Switchboard/sw02-0224_DiAML-MultiTab.csv')
+    files.append('csv/Switchboard/sw03-0304_DiAML-MultiTab1.csv')
 
-    dict_to_yaml(file_out, data)
+    #MapTask
+    files.append('csv/MapTask/q1ec5_DiAML-MultiTab.csv')
+    files.append('csv/MapTask/q1ec6_DiAML-MultiTab.csv')
+
+    #DBOX
+    files.append('csv/DBOX/diana_DiAML-MultiTab-4.csv')
+    files.append('csv/DBOX/eleanor_DiAML-MultiTab.csv')
+    files.append('csv/DBOX/rihanna_DiAML-MultiTab.csv')
+    files.append('csv/DBOX/venus_DiAML-MultiTab.csv')
+    files.append('csv/DBOX/washington_DiAML-MultiTab.csv')
+
+
+    for file in files:
+        file_out = file[:-3] + 'yaml'
+
+        data = cvs_to_dict(file)
+
+        # print(data[1].keys())
+
+        dict_to_yaml(file_out, data)
