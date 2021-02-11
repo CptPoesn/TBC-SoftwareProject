7cdd43061ef62c1fec6bab90d8f6fc45afa61e5d
TheresaSchmidt <36304889+TheresaSchmidt@users.noreply.github.com>
2021-02-11 18:25:17 +0100

    fixed step update bug - please double-check

--- Jan23_Rasa/our_dialogue_manager_test6.py
+++ Jan23_Rasa/our_dialogue_manager_test6.py
@@ -18,6 +18,8 @@ import math
 import re
 import string
 from nltk.tokenize import word_tokenize
+from collections import defaultdict
+from operator import itemgetter
 
 
 # functions################################################################
@@ -62,17 +64,17 @@ def get_utterance_predictor_and_tokenizer(predictor="huggingtweets/ppredictors")
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
     return generator, tokenizer
 
-def predict_with_score(input, generator, tokenizer):
+def predict_with_score(input, generator, tokenizer, num_predictions=5):
 
     # make model and generate output
     input_ids = tokenizer(input, return_tensors="pt").input_ids # "pt" stands for pytorch; could also be "tf"
-    generated_outputs = generator.generate(input_ids, do_sample=True, num_return_sequences=5, output_scores=True)
+    generated_outputs = generator.generate(input_ids, do_sample=True, num_return_sequences=num_predictions, output_scores=True)
 
     # gen_sequences has shape [5, 15] TODO why 15????
     gen_sequences = generated_outputs.sequences[:, input_ids.shape[-1]:]
 
     # stack the logits generated at each step to a tensor and transform
-    # logits to probs
+    # logits to probs TODO: Why do we sometimes get an empty tensor here??
     probs = torch.stack(generated_outputs.scores, dim=1).softmax(-1)  # -> shape [5, 15, vocab_size]
 
     # now we need to collect the probability of the generated token
@@ -93,14 +95,41 @@ def predict_with_score(input, generator, tokenizer):
     return [{"generated_text":tokenizer.decode(x), "score":y}
             for x,y in zip(gen_sequences, unique_normed_prob_per_sequence)]
 
+def sigmoid(x):
+    # actually quicker than pre-implemented, according to https://stackoverflow.com/questions/3985619/how-to-calculate-a-logistic-sigmoid-function-in-python
+    return 1 / (1 + math.exp(-x))
 
 def weight_by_utterance_probability(pred_score, scaling_weight_utterance_prediction_score):
     # TODO (urgent): define meaningful heuristic!!!
     #return 1
     try:
+        # tried sigmoid here (to keep total value below 1.0) but didn't find good values for other hyper-parameters
         return float(scaling_weight_utterance_prediction_score)/math.log(pred_score,10) # i.e. 10/num_decimal_zeroes for scaling_weight_utterance_prediction_score=-10
     except ValueError:
         return 1/1000 # TODO: double-check: this should be a very small number
+
+
+def get_pred_text(input_so_far, predfull_text, predicted):
+    print("predfull_text: ", predfull_text)
+    punc = "[" + ".?!" + "]"  # string.punctuation
+    allpunc = "[" + string.punctuation + "]"
+    if predicted == "by_sentend":
+        pred_text = re.split(punc, predfull_text)[0].strip()  # split by sentence end punc
+    elif predicted == "by_allpunc":
+        pred_text = re.split(allpunc, predfull_text)[0].strip()  # split by all punc
+    elif predicted == "by_fulltext":
+        pred_text = predfull_text.strip()  # use full text
+
+    pred_text = " ".join(input_so_far + [pred_text]) # append the newly predicted text to what we already have as input
+    pred_text = pred_text.replace("<|endoftext|>","") # str.removesuffix is only implemented in python>=3.9
+    print("prediction item: ", pred_text)
+
+    return pred_text
+
+def earliest_locking_time(trp_list):
+    user_utterance, predicted_utterance, prediction = min(trp_list, key=(lambda x: (len(x[0]), -x[2][1])))
+    intent,score = prediction
+    return user_utterance, intent, score, predicted_utterance
 
 
 # def intent_cumulative(intent, confi):
@@ -113,91 +142,106 @@ def weight_by_utterance_probability(pred_score, scaling_weight_utterance_predict
 
 # generate up to first sentence-end marker (. or ? or !)
 
-def get_pred_text(input_so_far, predfull_text, predicted):
-    print("predfull_text: ", predfull_text)
-    punc = "[" + ".?!" + "]"  # string.punctuation
-    allpunc = string.punctuation
-    if predicted == "by_sentend":
-        pred_text = re.split(punc, predfull_text)[0].strip()  # split by sentence end punc
-    elif predicted == "by_allpunc":
-        pred_text = predfull_text.split(allpunc)[0].strip()  # split by all punc
-    elif predicted == "by_fulltext":
-        pred_text = predfull_text.strip()  # use full text
-    pred_text = " ".join(input_so_far + [pred_text]) # append the newly predicted text to what we already have as input
-    print("prediction item: ", pred_text)
-
-    return pred_text
-
-
-def earliest_locking_time(trp_list):
-    user_utterance, predicted_utterance, prediction = sorted(trp_list, key=(lambda x: len(x[0])))[0]
-    intent, score = prediction
-    return user_utterance, intent, predicted_utterance
-
 
 def main(tokenized_msg, generator, tokenizer, rasa_model, threshold,
-         update_weight_timesteps, predicted, scaling_weight_utterance_prediction_score):
+         update_weight_timesteps, predicted, scaling_weight_utterance_prediction_score,
+         average=False, num_utterance_predictions = 5): #TODO implement num_utt_pred
     cumu_msg = []  # cumulated message
-    intent_dict = dict()
-    response_word_at_trp = ""
-    response_intent_at_trp = (0, 0)
+    intent_dict = defaultdict(int)
     trp_list = list()
-    earliest_word = ""
-    utterance_prediction_at_trp = ""
 
     for word in tokenized_msg:
         cumu_msg.append(word)  # take one more word from full msg
         out = " ".join(cumu_msg)
         print("current output: ", out)
 
-        predict = predict_with_score(out, generator, tokenizer) # generate 5 text (tweet) predictions
+        _intent_dict = defaultdict(list)
+
+        predict = predict_with_score(out, generator, tokenizer, num_predictions=num_utterance_predictions) # generate 5 text (tweet) predictions
+        # TODO: make the number of predictions a variable
+
         for item in predict:  # for each prediction text
-            pred_text = get_pred_text(cumu_msg, item["generated_text"], predicted)
-
-            # Get intents ranking
-
-            all_result = process_input(pred_text, rasa_model)  # classify intent # TODO: possibly double-check whether pred_text==""
-            # or maybe empty string means "end of sentence", i.e. the gpt2 prediction starts with ". NextSentence"
-            # TODO: break when the utterance prediction we use is ""
-            item_intent_dis = intent_prob(all_result)
-
             pred_score = item["score"] # score for utterance prediction
+            utt_score = weight_by_utterance_probability(pred_score, scaling_weight_utterance_prediction_score)
+            pred_text = get_pred_text(cumu_msg, item["generated_text"], predicted)
+            print("pred text: ", pred_text)
 
+            # Get intents ranking for predicted utterance
+            all_result = process_input(pred_text, rasa_model)  # classify intent
+            # TODO: possibly double-check whether pred_text==""
+            # or maybe empty string means "end of sentence", i.e. the gpt2 prediction starts with ". NextSentence"
+            # TODO: break when the utterance prediction we use is "" ?
+            item_intent_dis = intent_prob(all_result) # item_intent_dis = list of (intent,confi)
+
+            """
+            proposed bug fix: 
+            """
+            for intent,confi in item_intent_dis:
+                score = float(confi) * utt_score # In Gervits they use utt_score as threshold instead of weight
+                #weight = 0.5
+                #score = float(confi) * (1-weight) + sigmoid(utt_score) * (weight) # curiously, this results int empty tensor list in predict_with_score
+                #score = sigmoid(float(confi) + utt_score) #TODO: why do this and the above line have this strange error?
+                _intent_dict[intent].append((pred_text, score))
+            #print(sorted(intent_dict.items(), key=operator.itemgetter(1), reverse=True))
+
+        # make average
+        for intent, predictions in _intent_dict.items():
+            # _intent_dict[intent] : list of (predicted_utterance, intent_score) tuples
+
+            # get utterance and score TODO: score must be weighted by utterance weight
+            highest_ranking_utterance, score = max(predictions, key=itemgetter(1))
+            if average:
+                # replace score by average over the scores of all utterance predictions at this time step
+                average = sum([x[1] for x in predictions]) / 5.0  # score = average_score
+                weight = 0.5
+                score = average * (1- weight) + score * weight
+
+            intent_dict[intent] = intent_dict[intent] * (
+                        1 - update_weight_timesteps) + score * update_weight_timesteps  # in Gervits et al.: update_weight_timesteps=1.0
+            if score >= threshold:
+                trp_list.append((out, highest_ranking_utterance, (intent, score)))
+
+        # compute output at this time step
+        if trp_list:
+            break
+
+        print("\n ------------------------- \n")
+
+        """ bugged
+            
             for (intent, confi) in item_intent_dis:
                 if intent in intent_dict:
+                    # add to variable
                     intent_update = intent_dict[intent] * (1 - update_weight_timesteps) + float(confi) * update_weight_timesteps # TODO make variable
                     intent_update = intent_update * weight_by_utterance_probability(pred_score, scaling_weight_utterance_prediction_score) # TODO get meaningful weighting heuristic
                     intent_dict[intent] = intent_update
                     if intent_update >= threshold:
                         if earliest_word == "":
                             earliest_word = out
-                        response_word_at_trp = out
-                        utterance_prediction_at_trp = pred_text
-                        response_intent_at_trp = sorted(intent_dict.items(), key=operator.itemgetter(1), reverse=True)[0]
-                        trp_list.append((out, utterance_prediction_at_trp, response_intent_at_trp))
+                        response_word_at_locking_time = out
+                        utterance_prediction_at_locking_time = pred_text
+                        print("pred text saved: ", pred_text)
+                        response_intent_at_locking_time = max(intent_dict.items(), key=operator.itemgetter(1) )
+                        trp_list.append((out, utterance_prediction_at_locking_time, response_intent_at_locking_time))
                 else:
                     intent_dict[intent] = float(confi) * weight_by_utterance_probability(pred_score, scaling_weight_utterance_prediction_score)
-            print(sorted(intent_dict.items(), key=operator.itemgetter(1), reverse=True))
-        print("\n ------------------------- \n")
+                    
+        """
 
 
-    # TODO: edit after Clara's update / @Clara: double-check
-    if not response_word_at_trp:
-        response_word_at_trp = out
-    if response_intent_at_trp == (0,0):
-        response_intent_at_trp = sorted(intent_dict.items(), key=operator.itemgetter(1), reverse=True)[0]
-    if not utterance_prediction_at_trp:
-        utterance_prediction_at_trp = pred_text
+    # If threshold hasn't been reached,
+    #   choose best intent after reading in the whole input
     if not trp_list:
-        trp_list.append((response_word_at_trp, utterance_prediction_at_trp, response_intent_at_trp))
-
-    print("full message: ", " ".join(tokenized_msg))
-    print("earliest possible response point: ", response_word_at_trp)
-    print("top intent and confi at earliest possible response point: ", response_intent_at_trp)
-    print("trp list: ", trp_list)
+        best_intent,best_score = max(intent_dict.items(), key=itemgetter(1))
+        trp_list.append((out, pred_text, (best_intent, best_score) ))
 
     # extract return values from trp-list
-    msg_at_locking_time, p_intent, p_utterance = earliest_locking_time(trp_list)
+    msg_at_locking_time, p_intent, score, p_utterance = earliest_locking_time(trp_list)
+
+    print("full message: ", " ".join(tokenized_msg))
+    print("earliest possible response point: ", msg_at_locking_time)
+    print("top intent and score at earliest possible response point: ", p_intent, score)
+    print("trp list: ", trp_list)
 
     return msg_at_locking_time, p_intent, p_utterance
 
